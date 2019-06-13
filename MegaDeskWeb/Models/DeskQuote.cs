b45cef9d094018ae27c7e2841cb93d6103fa4a3e
andersonnw <and14094@byui.edu>
2019-06-13 10:07:50 -0600

    addded classes

---
+++ MegaDeskWeb/Models/DeskQuote.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MegaDeskWeb.Models
+{
+    public class DeskQuote
+    {
+        public int ID { get; set; }
+
+    }
+}
