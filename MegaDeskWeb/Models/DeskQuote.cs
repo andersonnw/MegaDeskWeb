019ba9ff724ab815ec52b1d3af44d997cab1a687
andersonnw <and14094@byui.edu>
2019-06-13 10:22:37 -0600

    Cretaed the model

--- MegaDeskWeb/Models/DeskQuote.cs
+++ MegaDeskWeb/Models/DeskQuote.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,14 @@ namespace MegaDeskWeb.Models
     public class DeskQuote
     {
         public int ID { get; set; }
+
+        public string CustomerName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        public Desk Desk { get; set; }
+        public float Price { get; set; }
+        public Shipping Shipping { get; set; }
 
     }
 }
