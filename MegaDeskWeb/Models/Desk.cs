57d3d7dcaa12809f838c4a40a5210a66287290a1
Wade Gossage <kiba_hlv@yahoo.com>
2019-06-15 15:20:17 -0600

    Cues Update

--- MegaDeskWeb/Models/Desk.cs
+++ MegaDeskWeb/Models/Desk.cs
@@ -9,9 +9,9 @@ namespace MegaDeskWeb.Models
     public class Desk
     {
         public int ID { get; set; }
-        [RegularExpression(@"^(4[0-8]|1[2-9])$", ErrorMessage = "Please enter a value between 12 and 48")] 
+        [RegularExpression(@"^(4[0-8]|3[0-9]|2[0-9]|1[2-9])$", ErrorMessage = "Please enter a value between 12 and 48")] 
         public int Depth { get; set; }
-        [RegularExpression(@"^(9[0-6]|2[4-9])$", ErrorMessage = "Please enter a value between 24 and 96")]
+        [RegularExpression(@"^(9[0-6]|8[0-9]|7[0-9]|6[0-9]|5[0-9]|4[0-9]|3[0-9]|2[4-9])$", ErrorMessage = "Please enter a value between 24 and 96")]
         public int Width { get; set; }
         public Desktop Material { get; set; }
         [RegularExpression(@"^([0]|[1-7])$", ErrorMessage = "Please enter a value between 0 and 7")]
