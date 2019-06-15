e88bc3b77dfa7f64e93a1dd571bee73e1b8e85fb
andersonnw <and14094@byui.edu>
2019-06-15 13:04:19 -0600

    Populated the shipping drop down

--- MegaDeskWeb/Pages/DeskQuotes/Create.cshtml.cs
+++ MegaDeskWeb/Pages/DeskQuotes/Create.cshtml.cs
@@ -14,6 +14,9 @@ namespace MegaDeskWeb.Pages.DeskQuotes
     {
         private readonly MegaDeskWeb.Models.MegaDeskWebContext _context;
 
+        public List<SelectListItem> shippings { get; set; }
+        public List<SelectListItem> materials { get; set; }
+
         public CreateModel(MegaDeskWeb.Models.MegaDeskWebContext context)
         {
             _context = context;
@@ -21,12 +24,29 @@ namespace MegaDeskWeb.Pages.DeskQuotes
 
         public IActionResult OnGet()
         {
+            shippings = _context.Shipping.Select(a => new SelectListItem
+            {
+                Value = a.ID.ToString(),
+                Text = a.Type
+
+            }).ToList();
+
+            materials = _context.Desktop.Select(a => new SelectListItem
+            {
+                Value = a.ID.ToString(),
+                Text = a.Type
+
+            }).ToList();
+
             return Page();
         }
 
         [BindProperty]
         public DeskQuote DeskQuote { get; set; }
-        public IList<Shipping> shippings { get; set; }
+        
+        
+    
+
 
         public async Task<IActionResult> OnPostAsync()
         {
@@ -36,7 +56,7 @@ namespace MegaDeskWeb.Pages.DeskQuotes
             }
 
             _context.DeskQuote.Add(DeskQuote);
-            shippings = await _context.Shipping.ToListAsync();
+
 
             await _context.SaveChangesAsync();
 
